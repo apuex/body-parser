aab1cb5cbefed4644a6dcceeabcb8fd8c41638b3
Wangxy <xtwxy@hotmail.com>
2019-03-28 14:46:30 +0800

    add tests.

---
+++ src/test/scala/com/github/apuex/play/bodyparser/GsonBodySpec.scala
@@ -0,0 +1,13 @@
+package com.github.apuex.play.bodyparser
+
+import org.scalatest._
+
+class GsonBodySpec extends FlatSpec with Matchers {
+  "A GsonBody parser" should "parse json to protobuf message" in {
+
+  }
+
+  "A GsonBody printer" should "print protobuf message to json" in {
+
+  }
+}
